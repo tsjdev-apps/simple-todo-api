ff13c6d3e55170acd32a51180975f3f07f6cfa60
Sebastian Jensen <apps@tsjdev-apps.de>
2019-05-04 19:49:09 +0200

    - add methods for first version of todocontroller

--- TodoApi/TodoApi/Controllers/TodoController.cs
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Models;
 
@@ -9,5 +11,70 @@ namespace TodoApi.Controllers
     public class TodoController : ControllerBase
     {
         private static List<TodoItem> _todoItems = new List<TodoItem>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_todoItems);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var todoItem = GetTodoItem(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        public IActionResult Add(TodoItemDto todoItemDto)
+        {
+            var todoItem = new TodoItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = todoItemDto.Content,
+                IsDone = todoItemDto.IsDone,
+                TimeStamp = DateTime.Now
+            };
+
+            _todoItems.Add(todoItem);
+
+            return Ok(todoItem);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todoItem = GetTodoItem(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            _todoItems.Remove(todoItem);
+
+            return Ok();
+        }
+
+        [HttpPut("status/{id}")]
+        public IActionResult Update(string id)
+        {
+            var todoItem = GetTodoItem(id);
+
+            if (todoItem == null)
+                return NotFound();
+
+            todoItem.IsDone = !todoItem.IsDone;
+            todoItem.TimeStamp = DateTime.Now;
+
+            return Ok(todoItem);
+        }
+
+        private TodoItem GetTodoItem(string id)
+        {
+            return _todoItems.FirstOrDefault(t => t.Id == id);
+        }
     }
 }
